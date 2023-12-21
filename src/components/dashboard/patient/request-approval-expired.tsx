07963623a1ee86631d2f4ea59eb7a5a10cf50cda
victor Bello <victor.bello@tizeti.com>
2023-12-21 09:22:29 +0100

    request approval feature added

--- src/components/dashboard/patient/request-approval-expired.tsx
+++ src/components/dashboard/patient/request-approval-expired.tsx
@@ -13,7 +13,7 @@ export default function RequestApprovalExpired() {
     return (
         <main className="h-full flex flex-col justify-center items-center">
 
-            <Card className="w-[80%] md:w-[60%] flex flex-col items-center">
+            <Card className="w-[86%] md:w-[60%] flex flex-col items-center">
                 <CardHeader className="text-center space-y-4">
                     <CardTitle className="text-xl">ACCEPT OR APPROVE </CardTitle>
                     <CardDescription className="text-lg text-red-400">Link is Expired</CardDescription>
