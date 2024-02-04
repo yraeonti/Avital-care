f4bdc93378c23acd9d6d9608a70661c2f081a8a8
victor Bello <victor.bello@tizeti.com>
2024-02-04 23:23:37 +0100

    forgot password implementation

---
+++ src/app/middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+    // retrieve the current response
+    const res = NextResponse.next()
+
+    // if the incoming is for the desired API endpoint
+    if (req.nextUrl.pathname === '/publicdoctors') {
+        res.headers.append('Access-Control-Allow-Credentials', "true")
+        res.headers.append('Access-Control-Allow-Origin', '*')
+        res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+        res.headers.append('Access-Control-Allow-Headers', 'Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date')
+    }
+
+    return res
+}
+
+// specify the path regex to apply the middleware to
+export const config = {
+    matcher: '/api/:path*',
+}
