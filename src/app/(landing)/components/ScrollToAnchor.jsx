3f525f9e00575d2ef014e8bacae38bbdfc7e4ac7
moegreene5 <agbasiifeanyi@gmail.com>
2024-02-12 12:21:52 +0100

    last commit

--- src/app/(landing)/components/ScrollToAnchor.jsx
+++ src/app/(landing)/components/ScrollToAnchor.jsx
@@ -2,16 +2,16 @@
 import { useEffect } from "react";
 
 function ScrollToAnchor() {
-  useEffect(() => {
-    const hash = window.location.hash.substring(1);
-    if (hash) {
-      window.history.replaceState(
-        null,
-        document.title,
-        window.location.pathname
-      );
-    }
-  }, []);
+  // useEffect(() => {
+  //   const hash = window.location.hash.substring(1);
+  //   if (hash) {
+  //     window.history.replaceState(
+  //       null,
+  //       document.title,
+  //       window.location.pathname
+  //     );
+  //   }
+  // }, []);
 
   return null;
 }
