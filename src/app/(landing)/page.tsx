b6265fb6a1668f034b84e5e970b129550f75af51
kenix2 <odochkenneth72@gmail.com>
2025-04-07 23:09:09 +0300

    page.tsx

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,7 +1,4 @@
-"use client";
-
-import Link from "next/link";
-
+"use client"
 import About from "./components/About";
 import Banner from "./components/Banner";
 import Carousel from "./components/Carousel";
@@ -20,16 +17,6 @@ export default function Page() {
             <Carousel />
             <HomeBanner />
             <HomeIcons />
-
-            {/* 🔗 Added the new link here */}
-            <div className="text-center my-6">
-                <Link href="/research">
-                    <span className="text-blue-600 hover:underline font-semibold text-lg">
-                        Avita Health Research and Education
-                    </span>
-                </Link>
-            </div>
-
             <Faq />
             <About />
         </>
