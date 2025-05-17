8fa5250a452a78d7a907e65552e6596b3dc28615
kenix2 <odochkenneth72@gmail.com>
2025-05-17 17:14:11 +0300

    page.tsx

--- src/app/volunteer/page.tsx
+++ src/app/volunteer/page.tsx
@@ -28,7 +28,7 @@ const countries = {
     accommodation: "Volunteer dorms or homestay with meals",
   },
   "Sierra Leone": {
-    image: "se.jpeg",
+    image: "/se.jpeg",
     duration: "2-20 weeks",
     cost: "From 2300 USD",
     price: 230000,
